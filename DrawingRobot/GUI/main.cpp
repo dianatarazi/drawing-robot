9285018d7349279532c2ec4fc50f1667de48949c
dianatarazi <diana.tarazi15@ncf.edu>
2017-05-11 18:28:58 -0400

    Segmentation fault

--- DrawingRobot/GUI/main.cpp
+++ DrawingRobot/GUI/main.cpp
@@ -1,7 +1,116 @@
-#include <iostream>
+/*
+ * Copyright (C) 2008 Emweb bvba, Heverlee, Belgium.
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <Wt/WApplication>
+#include <Wt/WBreak>
+#include <Wt/WContainerWidget>
+#include <Wt/WLineEdit>
+#include <Wt/WPushButton>
+#include <Wt/WText>
+
+
+// c++0x only, for std::bind
+// #include <functional>
+
+using namespace Wt;
+
+/*
+ * A simple hello world application class which demonstrates how to react
+ * to events, read input, and give feed-back.
+ */
+class HelloApplication : public WApplication
+{
+public:
+  HelloApplication(const WEnvironment& env);
+
+private:
+  WLineEdit *nameEdit_;
+  WText *intro_;
+
+  void greetAndStart(WPushButton *button);
+};
+
+/*
+ * The env argument contains information about the new session, and
+ * the initial request. It must be passed to the WApplication
+ * constructor so it is typically also an argument for your custom
+ * application constructor.
+*/
+HelloApplication::HelloApplication(const WEnvironment& env)
+  : WApplication(env)
+{
+  setTitle("Drawing Robot - GUI");                               // application title
+
+  root()->addWidget(new WText("What's your name?"));  // show some text
+  nameEdit_ = new WLineEdit(root());                     // allow text input
+  nameEdit_->setFocus();                                 // give focus
+
+  WPushButton *button
+    = new WPushButton("Start", root());              // create a button
+  button->setMargin(5, Left);                            // add 5 pixels margin
+
+  root()->addWidget(new WBreak());                       // insert a line break
+
+  intro_ = new WText(root());                         // empty text
+
+  /*
+   * Connect signals with slots
+   *
+   * - simple Wt-way
+   */
+  button->clicked().connect(boost::bind(&HelloApplication::greetAndStart, this, button));
+
+  /*
+   * - using an arbitrary function object (binding values with boost::bind())
+   */
+  nameEdit_->enterPressed().connect
+    (boost::bind(&HelloApplication::greetAndStart, this, button));
+
+  /*
+   * - using a c++0x lambda:
+   */
+  // button->clicked().connect(std::bind([=]() { 
+  //       greeting_->setText("Hello there, " + nameEdit_->text());
+  // }));
+}
+
+void HelloApplication::greetAndStart(WPushButton *button)
+{
+  /*
+   * Update the text, using text input into the nameEdit_ field.
+   */
+  intro_->setText("Hello, " + nameEdit_->text() + "! Welcome to the Drawing Robot User Interface! Please draw a picture using the canvas below!" );
+  button->setDisabled(true);
+}
+
+
+
+WApplication *createApplication(const WEnvironment& env)
+{
+  /*
+   * You could read information from the environment to decide whether
+   * the user has permission to start a new application
+   */
+  return new HelloApplication(env);
+}
+
+
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+  /*
+   * Your main method may set up some shared resources, but should then
+   * start the server application (FastCGI or httpd) that starts listening
+   * for requests, and handles all of the application life cycles.
+   *
+   * The last argument to WRun specifies the function that will instantiate
+   * new application objects. That function is executed when a new user surfs
+   * to the Wt application, and after the library has negotiated browser
+   * support. The function should return a newly instantiated application
+   * object.
+   */
+  return WRun(argc, argv, &createApplication);
 }
